eec35ed7891040047b04b69da554ed3a09f3910f
Gavin Melling <gavin@pixelinspiration.com>
2015-09-29 15:12:04 +0100

    Added test script for touch screen

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,36 @@ $(document).ready( function(){
 	var modelApp = new AppModel();
 	ko.applyBindings( modelApp );
 
+	var $ul = $("ul"), $doc = $(document);
+	var evtTouchStart = null;
+
+	$doc.on("touchstart", function( evt ){
+		evtTouchStart = evt;
+	});
+
+	$ul.on("scroll",function(){
+		console.log("scroll", arguments);
+
+		if( !$ul.hasClass("scrolling") ){
+			$ul.addClass("scrolling");
+			$doc.on( "touchend", onScrollStop );
+		}
+
+		if( evtTouchStart ){
+			evtTouchStart.preventDefault();
+			evtTouchStart = null;
+		}
+	});
+
+	function onScrollStop( evt ){
+		evt.preventDefault();
+		$doc.unbind("touchend",onScrollStop );
+		//$ul.removeClass("scrolling");
+		evtTouchStart = null;
+	}
+
+
+
 } );
 
 function AppModel(){
---
+++ start.bat
@@ -0,0 +1,4 @@
+jade --watch *.jade --pretty &
+stylus -w -u nib public/css/index.styl &
+
+python -m SimpleHTTPServer
